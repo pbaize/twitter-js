5acb2dac655f890b288afe0811a8655b840e20c0
Joe Bosman <joseph.a.bosman@gmail.com>
2016-08-02 15:35:02 -0400

    Added data array and add, list, and find functions.

---
+++ tweetBank.js
@@ -0,0 +1,22 @@
+var _ = require("lodash");
+
+
+var data = [];
+
+function add (name, content) {
+  data.push({ name: name, content: content });
+}
+
+function list () {
+  return _.cloneDeep(data);
+}
+
+function find (properties) {
+  return _.cloneDeep(_.filter(data, properties));
+}
+
+module.exports = { 
+	add: add, 
+	list: list, 
+	find: find 
+};
