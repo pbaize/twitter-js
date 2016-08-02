bfd40669e3e58415a1dac346f1aa8fe8bf339320
Pierre Baize <pierrebaize@gmail.com>
2016-08-02 15:12:28 -0400

    set engine

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
-var express = require( 'express' );
+var express = require('express');
 var app = express(); // creates an instance of an express application
 var chalk = require('chalk');
+var swig = require('swig');
+var locals;
 
-app.listen(3000 ,function(){
-	console.log('server listening');
+var people =[{
+                name: 'Gandalf'
+            }, {
+                name: 'Gollum'
+            }, {
+                name: 'Hermione'
+            }];
+
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// disables caching
+app.set('view cache', false);
+swig.setDefaults({ cache: false });
+
+
+app.listen(3000, function() {
+    console.log('server listening');
 });
 
-app.use(function( req, res, next){
-	console.log(chalk.blue("Server message"));
-	next();
+app.use(function(req, res, next) {
+    console.log(chalk.blue("Server message"));
+    next();
 });
 
-app.use('/news', function( req, res, next){
-	console.log(chalk.green("News message"));
-	next();
+app.get('/people', function(req, res, next) {
+    res.render('index', {
+            title: 'An Example',
+            people: people
+        }
+    );
 });
 
-app.get('/', function(req, res){
-	res.send( ["GET /", res.statusCode].join(" ") );
+app.use('/news', function(req, res, next) {
+    console.log(chalk.green("News message"));
+    next();
 });
 
-app.get('/news', function(req, res){
-	res.send("fuck you, world!");
+app.get('/', function(req, res) {
+    res.send(["GET /", res.statusCode].join(" "));
 });
 
-
-
+app.get('/news', function(req, res) {
+    res.send("fuck you, worldß!");
+});
